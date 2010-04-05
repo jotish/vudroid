146cd643bbfd2ab970ef65c3c370975a040fecdc
pavel.tiunov <pavel.tiunov@aae08a87-be0d-dedf-da58-693b0a99159c>
2010-04-05 21:06:08 +0000

    Document View redesign

--- trunk/src/main/java/org/vudroid/core/views/PageViewZoomControls.java
+++ trunk/src/main/java/org/vudroid/core/views/PageViewZoomControls.java
@@ -1,53 +1,57 @@
 package org.vudroid.core.views;
 
+import android.R;
 import android.content.Context;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
-import android.widget.ZoomControls;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import org.vudroid.core.events.BringUpZoomControlsListener;
-import org.vudroid.core.events.ZoomListener;
 import org.vudroid.core.models.ZoomModel;
 
-public class PageViewZoomControls extends ZoomControls implements BringUpZoomControlsListener, ZoomListener
+public class PageViewZoomControls extends LinearLayout implements BringUpZoomControlsListener
 {
     private int atomicShowCounter = 0;
-    private final ZoomModel zoomModel;
 
     public PageViewZoomControls(Context context, final ZoomModel zoomModel)
     {
         super(context);
         hide();
-        setOnZoomInClickListener(new OnClickListener()
+        final LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setGravity(Gravity.CENTER_VERTICAL);
+        linearLayout.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        final SeekBar seekBar = new SeekBar(context);
+        seekBar.setMax(300);
+        seekBar.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
         {
-            public void onClick(View view)
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b)
             {
-                zoomModel.increaseZoom();
+                zoomModel.setZoom(1.0f + i / 100.0f);
+                bringUpZoomControls();
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+                zoomModel.commit();
             }
         });
-        setOnZoomOutClickListener(new OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                zoomModel.decreaseZoom();
-            }
-        });
-        final ToggleButton button = new ToggleButton(context);
-        button.setTextOn("Scroll On");
-        final String s = "Scroll Off";
-        button.setTextOff(s);
-        button.setText(s);
-        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b)
-            {
-                zoomModel.setHorizontalScrollEnabled(b);
-            }
-        });
-        addView(button, 0);
-        this.zoomModel = zoomModel;
-        setZoomEnabled();
+
+        setOrientation(LinearLayout.HORIZONTAL);
+        setGravity(Gravity.BOTTOM);
+        final ImageView imageView = new ImageView(context);
+        imageView.setImageResource(R.drawable.ic_menu_zoom);
+        linearLayout.addView(imageView);
+        linearLayout.addView(seekBar);
+        addView(linearLayout);
     }
 
     @Override
@@ -89,13 +93,21 @@ public class PageViewZoomControls extends ZoomControls implements BringUpZoomCon
         }, 2000);
     }
 
-    public void zoomChanged(float newZoom, float oldZoom)
+    private void show()
     {
-        setZoomEnabled();
+        fade(View.VISIBLE, 0.0f, 1.0f);
     }
 
-    private void setZoomEnabled()
+    private void hide()
     {
-        setIsZoomOutEnabled(zoomModel.canDecrement());
+        fade(View.GONE, 1.0f, 0.0f);
+    }
+
+    private void fade(int visibility, float startAlpha, float endAlpha)
+    {
+        AlphaAnimation anim = new AlphaAnimation(startAlpha, endAlpha);
+        anim.setDuration(500);
+        startAnimation(anim);
+        setVisibility(visibility);
     }
 }
