4395246ceedb81ff7730d3ce8305d02998c87d2f
pavel.tiunov@gmail.com <pavel.tiunov@gmail.com@aae08a87-be0d-dedf-da58-693b0a99159c>
2010-12-11 20:55:04 +0000

    Artifact lines between page slices fix

--- src/main/java/org/vudroid/core/Page.java
+++ src/main/java/org/vudroid/core/Page.java
@@ -6,15 +6,16 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.text.TextPaint;
 
-class Page
-{
+class Page {
     final int index;
     RectF bounds;
     private PageTreeNode node;
     private DocumentView documentView;
+    private final TextPaint textPaint = textPaint();
+    private final Paint fillPaint = fillPaint();
+    private final Paint strokePaint = strokePaint();
 
-    Page(DocumentView documentView, int index)
-    {
+    Page(DocumentView documentView, int index) {
         this.documentView = documentView;
         this.index = index;
         node = new PageTreeNode(documentView, new RectF(0, 0, 1, 1), this, 1, null);
@@ -22,66 +23,70 @@ class Page
 
     private float aspectRatio;
 
-    float getPageHeight(int mainWidth, float zoom)
-    {
+    float getPageHeight(int mainWidth, float zoom) {
         return mainWidth / getAspectRatio() * zoom;
     }
 
-    public int getTop()
-    {
+    public int getTop() {
         return Math.round(bounds.top);
     }
 
-    public void draw(Canvas canvas)
-    {
-        if (!isVisible())
-        {
+    public void draw(Canvas canvas) {
+        if (!isVisible()) {
             return;
         }
-        final Paint rectPaint = new Paint();
-        rectPaint.setColor(Color.GRAY);
-        rectPaint.setStrokeWidth(4);
-        rectPaint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(bounds, rectPaint);
-        rectPaint.setColor(Color.BLACK);
-        rectPaint.setStyle(Paint.Style.STROKE);
-        canvas.drawRect(bounds, rectPaint);
+        canvas.drawRect(bounds, fillPaint);
 
+        canvas.drawText("Page " + (index + 1), bounds.centerX(), bounds.centerY(), textPaint);
+        node.draw(canvas);
+        canvas.drawLine(bounds.left, bounds.top, bounds.right, bounds.top, strokePaint);
+        canvas.drawLine(bounds.left, bounds.bottom, bounds.right, bounds.bottom, strokePaint);
+    }
+
+    private Paint strokePaint() {
+        final Paint strokePaint = new Paint();
+        strokePaint.setColor(Color.BLACK);
+        strokePaint.setStyle(Paint.Style.STROKE);
+        strokePaint.setStrokeWidth(2);
+        return strokePaint;
+    }
+
+    private Paint fillPaint() {
+        final Paint fillPaint = new Paint();
+        fillPaint.setColor(Color.GRAY);
+        fillPaint.setStyle(Paint.Style.FILL);
+        return fillPaint;
+    }
+
+    private TextPaint textPaint() {
         final TextPaint paint = new TextPaint();
         paint.setColor(Color.BLACK);
         paint.setAntiAlias(true);
         paint.setTextSize(24);
         paint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText("Page " + (index + 1), bounds.centerX(), bounds.centerY(), paint);
-        node.draw(canvas);
+        return paint;
     }
 
-    public float getAspectRatio()
-    {
+    public float getAspectRatio() {
         return aspectRatio;
     }
 
-    public void setAspectRatio(float aspectRatio)
-    {
-        if (this.aspectRatio != aspectRatio)
-        {
+    public void setAspectRatio(float aspectRatio) {
+        if (this.aspectRatio != aspectRatio) {
             this.aspectRatio = aspectRatio;
             documentView.invalidatePageSizes();
         }
     }
 
-    public boolean isVisible()
-    {
+    public boolean isVisible() {
         return RectF.intersects(documentView.getViewRect(), bounds);
     }
 
-    public void setAspectRatio(int width, int height)
-    {
+    public void setAspectRatio(int width, int height) {
         setAspectRatio(width * 1.0f / height);
     }
 
-    void setBounds(RectF pageBounds)
-    {
+    void setBounds(RectF pageBounds) {
         bounds = pageBounds;
         node.invalidateNodeBounds();
     }
