9fae467ad16a7216b25216e09363c067964472e9
pavel.tiunov@gmail.com <pavel.tiunov@gmail.com@aae08a87-be0d-dedf-da58-693b0a99159c>
2010-12-12 11:04:19 +0000

    Fix page number not always saved issue Fix scroll back when orientation changed on the last page Fix scroll back when zoom changed on the last page

--- src/main/java/org/vudroid/core/DocumentView.java
+++ src/main/java/org/vudroid/core/DocumentView.java
@@ -15,8 +15,7 @@ import org.vudroid.core.models.ZoomModel;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DocumentView extends View implements ZoomListener
-{
+public class DocumentView extends View implements ZoomListener {
     final ZoomModel zoomModel;
     private final CurrentPageModel currentPageModel;
     DecodeService decodeService;
@@ -33,8 +32,7 @@ public class DocumentView extends View implements ZoomListener
     private long lastDownEventTime;
     private static final int DOUBLE_TAP_TIME = 500;
 
-    public DocumentView(Context context, final ZoomModel zoomModel, DecodingProgressModel progressModel, CurrentPageModel currentPageModel)
-    {
+    public DocumentView(Context context, final ZoomModel zoomModel, DecodingProgressModel progressModel, CurrentPageModel currentPageModel) {
         super(context);
         this.zoomModel = zoomModel;
         this.progressModel = progressModel;
@@ -45,21 +43,17 @@ public class DocumentView extends View implements ZoomListener
         setFocusableInTouchMode(true);
     }
 
-    public void setDecodeService(DecodeService decodeService)
-    {
+    public void setDecodeService(DecodeService decodeService) {
         this.decodeService = decodeService;
     }
 
-    private void init()
-    {
-        if (isInitialized)
-        {
+    private void init() {
+        if (isInitialized) {
             return;
         }
         final int width = decodeService.getEffectivePagesWidth();
         final int height = decodeService.getEffectivePagesHeight();
-        for (int i = 0; i < decodeService.getPageCount(); i++)
-        {
+        for (int i = 0; i < decodeService.getPageCount(); i++) {
             pages.put(i, new Page(this, i));
             pages.get(i).setAspectRatio(width, height);
         }
@@ -68,18 +62,20 @@ public class DocumentView extends View implements ZoomListener
         goToPageImpl(pageToGoTo);
     }
 
-    private void goToPageImpl(final int toPage)
-    {
-        scrollTo(0, pages.get(toPage).getTop());        
+    private void goToPageImpl(final int toPage) {
+        scrollTo(0, pages.get(toPage).getTop());
     }
 
     @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt)
-    {
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
-        currentPageModel.setCurrentPageIndex(getCurrentPage());
-        if (inZoom)
-        {
+        // bounds could be not updated
+        post(new Runnable() {
+            public void run() {
+                currentPageModel.setCurrentPageIndex(getCurrentPage());
+            }
+        });
+        if (inZoom) {
             return;
         }
         // on scrollChanged can be called from scrollTo just after new layout applied so we should wait for relayout
@@ -90,90 +86,72 @@ public class DocumentView extends View implements ZoomListener
         });
     }
 
-    private void updatePageVisibility()
-    {
+    private void updatePageVisibility() {
         for (Page page : pages.values()) {
             page.updateVisibility();
         }
     }
 
-    public void commitZoom()
-    {
+    public void commitZoom() {
         for (Page page : pages.values()) {
             page.invalidate();
         }
         inZoom = false;
     }
 
-    public void showDocument()
-    {
+    public void showDocument() {
         // use post to ensure that document view has width and height before decoding begin
-        post(new Runnable()
-        {
-            public void run()
-            {
+        post(new Runnable() {
+            public void run() {
                 init();
                 updatePageVisibility();
             }
         });
     }
 
-    public void goToPage(int toPage)
-    {
-        if (isInitialized)
-        {
+    public void goToPage(int toPage) {
+        if (isInitialized) {
             goToPageImpl(toPage);
-        } else
-        {
+        } else {
             pageToGoTo = toPage;
         }
     }
 
-    public int getCurrentPage()
-    {
-        for (Map.Entry<Integer, Page> entry : pages.entrySet())
-        {
-            if (entry.getValue().isVisible())
-            {
+    public int getCurrentPage() {
+        for (Map.Entry<Integer, Page> entry : pages.entrySet()) {
+            if (entry.getValue().isVisible()) {
                 return entry.getKey();
             }
         }
         return 0;
     }
 
-    public void zoomChanged(float newZoom, float oldZoom)
-    {
+    public void zoomChanged(float newZoom, float oldZoom) {
         inZoom = true;
         stopScroller();
         final float ratio = newZoom / oldZoom;
-        scrollTo((int) ((getScrollX() + getWidth() / 2) * ratio - getWidth() / 2), (int) ((getScrollY() + getHeight() / 2) * ratio - getHeight() / 2));
         invalidatePageSizes();
+        scrollTo((int) ((getScrollX() + getWidth() / 2) * ratio - getWidth() / 2), (int) ((getScrollY() + getHeight() / 2) * ratio - getHeight() / 2));
         postInvalidate();
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent ev)
-    {
+    public boolean onTouchEvent(MotionEvent ev) {
         super.onTouchEvent(ev);
 
-        if (velocityTracker == null)
-        {
+        if (velocityTracker == null) {
             velocityTracker = VelocityTracker.obtain();
         }
         velocityTracker.addMovement(ev);
 
-        switch (ev.getAction())
-        {
+        switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 stopScroller();
                 lastX = ev.getX();
                 lastY = ev.getY();
-                if (ev.getEventTime() - lastDownEventTime < DOUBLE_TAP_TIME)
-                {
+                if (ev.getEventTime() - lastDownEventTime < DOUBLE_TAP_TIME) {
                     zoomModel.toggleZoomControls();
-                }
-                else
-                {
+                } else {
                     lastDownEventTime = ev.getEventTime();
                 }
                 break;
@@ -194,12 +172,9 @@ public class DocumentView extends View implements ZoomListener
     }
 
     @Override
-    public boolean dispatchKeyEvent(KeyEvent event)
-    {
-        if (event.getAction() == KeyEvent.ACTION_DOWN)
-        {
-            switch (event.getKeyCode())
-            {
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (event.getKeyCode()) {
                 case KeyEvent.KEYCODE_DPAD_RIGHT:
                     lineByLineMoveTo(1);
                     return true;
@@ -217,100 +192,81 @@ public class DocumentView extends View implements ZoomListener
         return super.dispatchKeyEvent(event);
     }
 
-    private void verticalDpadScroll(int direction)
-    {
-        scroller.startScroll(getScrollX(), getScrollY(), 0, direction * getHeight()/2);
+    private void verticalDpadScroll(int direction) {
+        scroller.startScroll(getScrollX(), getScrollY(), 0, direction * getHeight() / 2);
         invalidate();
     }
 
-    private void lineByLineMoveTo(int direction)
-    {
-        if (direction == 1 ? getScrollX() == getRightLimit() : getScrollX() == getLeftLimit())
-        {
-            scroller.startScroll(getScrollX(), getScrollY(), direction * (getLeftLimit()-getRightLimit()), (int) (direction * pages.get(getCurrentPage()).bounds.height()/50));
-        }
-        else
-        {
+    private void lineByLineMoveTo(int direction) {
+        if (direction == 1 ? getScrollX() == getRightLimit() : getScrollX() == getLeftLimit()) {
+            scroller.startScroll(getScrollX(), getScrollY(), direction * (getLeftLimit() - getRightLimit()), (int) (direction * pages.get(getCurrentPage()).bounds.height() / 50));
+        } else {
             scroller.startScroll(getScrollX(), getScrollY(), direction * getWidth() / 2, 0);
         }
         invalidate();
     }
 
-    private int getTopLimit()
-    {
+    private int getTopLimit() {
         return 0;
     }
 
-    private int getLeftLimit()
-    {
+    private int getLeftLimit() {
         return 0;
     }
 
-    private int getBottomLimit()
-    {
+    private int getBottomLimit() {
         return (int) pages.get(pages.size() - 1).bounds.bottom - getHeight();
     }
 
-    private int getRightLimit()
-    {
+    private int getRightLimit() {
         return (int) (getWidth() * zoomModel.getZoom()) - getWidth();
     }
 
     @Override
-    public void scrollTo(int x, int y)
-    {
+    public void scrollTo(int x, int y) {
         super.scrollTo(Math.min(Math.max(x, getLeftLimit()), getRightLimit()), Math.min(Math.max(y, getTopLimit()), getBottomLimit()));
         viewRect = null;
     }
 
-    RectF getViewRect()
-    {
-        if (viewRect == null)
-        {
+    RectF getViewRect() {
+        if (viewRect == null) {
             viewRect = new RectF(getScrollX(), getScrollY(), getScrollX() + getWidth(), getScrollY() + getHeight());
         }
         return viewRect;
     }
 
     @Override
-    public void computeScroll()
-    {
-        if (scroller.computeScrollOffset())
-        {
+    public void computeScroll() {
+        if (scroller.computeScrollOffset()) {
             scrollTo(scroller.getCurrX(), scroller.getCurrY());
         }
     }
 
     @Override
-    protected void onDraw(Canvas canvas)
-    {
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (Page page : pages.values())
-        {
+        for (Page page : pages.values()) {
             page.draw(canvas);
         }
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom)
-    {
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        invalidateScroll();
+        float scrollScaleRatio = getScrollScaleRatio();
         invalidatePageSizes();
+        invalidateScroll(scrollScaleRatio);
         commitZoom();
     }
 
-    void invalidatePageSizes()
-    {
-        if (!isInitialized)
-        {
+    void invalidatePageSizes() {
+        if (!isInitialized) {
             return;
         }
         float heightAccum = 0;
         int width = getWidth();
         float zoom = zoomModel.getZoom();
-        for (int i = 0; i < pages.size(); i++)
-        {
+        for (int i = 0; i < pages.size(); i++) {
             Page page = pages.get(i);
             float pageHeight = page.getPageHeight(width, zoom);
             page.setBounds(new RectF(0, heightAccum, width * zoom, heightAccum + pageHeight));
@@ -318,27 +274,29 @@ public class DocumentView extends View implements ZoomListener
         }
     }
 
-    private void invalidateScroll()
-    {
-        if (!isInitialized)
-        {
+    private void invalidateScroll(float ratio) {
+        if (!isInitialized) {
             return;
         }
         stopScroller();
         final Page page = pages.get(0);
-        if (page == null || page.bounds == null)
-        {
+        if (page == null || page.bounds == null) {
             return;
         }
-        final float v = zoomModel.getZoom();
-        float ratio = getWidth() * v / page.bounds.width();
         scrollTo((int) (getScrollX() * ratio), (int) (getScrollY() * ratio));
     }
 
-    private void stopScroller()
-    {
-        if (!scroller.isFinished())
-        {
+    private float getScrollScaleRatio() {
+        final Page page = pages.get(0);
+        if (page == null || page.bounds == null) {
+            return 0;
+        }
+        final float v = zoomModel.getZoom();
+        return getWidth() * v / page.bounds.width();
+    }
+
+    private void stopScroller() {
+        if (!scroller.isFinished()) {
             scroller.abortAnimation();
         }
     }
